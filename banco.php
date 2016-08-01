3ad9900fb4de4156a0aa55a502503690b9556fc1
Maciel Hogenn <maciel@merkadia.com.br>
2016-08-01 17:56:05 -0300

    Criado edição de tarefas

--- banco.php
+++ banco.php
@@ -35,3 +35,23 @@ function gravar_tarefa($conexao, $tarefa) {
 					{$tarefa['concluida']})";
 	mysqli_query($conexao, $sqlGravar);
 }
+
+function buscar_tarefa($conexao, $id){
+	$sqlBusca = 'SELECT * FROM tarefas WHERE id = ' . $id;
+	$resultado = mysqli_query($conexao, $sqlBusca);
+	return mysqli_fetch_assoc($resultado);
+}
+
+function editar_tarefa($conexao, $tarefa) {
+	$sqlEditar = "
+		UPDATE tarefas SET
+			nome = '{$tarefa['nome']}',
+			descricao = '{$tarefa['descricao']}',
+			prioridade = {$tarefa['prioridade']},
+			prazo = '{$tarefa['prazo']}',
+			concluida = {$tarefa['concluida']}
+		WHERE id = {$tarefa['id']}
+	";
+	
+	mysqli_query($conexao, $sqlEditar);
+}
