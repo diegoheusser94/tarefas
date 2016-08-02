1e0e8d6aee439e0c5c729650117820da2f0ba3a5
Maciel Hogenn <maciel@merkadia.com.br>
2016-08-02 12:57:39 -0300

    Capitulo 8 finalizado

--- banco.php
+++ banco.php
@@ -55,3 +55,8 @@ function editar_tarefa($conexao, $tarefa) {
 	
 	mysqli_query($conexao, $sqlEditar);
 }
+
+function remover_tarefa($conexao, $id){
+	$sqlRemover = "DELETE FROM tarefas WHERE id = {$id}";
+	mysqli_query($conexao, $sqlRemover);
+}
--- tabela.php
+++ tabela.php
@@ -21,6 +21,9 @@
 			<a href="editar.php?id=<?php echo $tarefa['id']; ?>">
 				Editar
 			</a>
+			<a href="remover.php?id=<?php echo $tarefa['id']; ?>">
+				Remover
+			</a>
 		</td>
 	</tr>
 	<?php endforeach; ?>
