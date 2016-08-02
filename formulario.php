e1cb44b90a60214fadb24a65f2f37e7b1af26154
Maciel Hogenn <maciel@merkadia.com.br>
2016-08-02 15:27:54 -0300

    Capitulo 9 finalizado

--- formulario.php
+++ formulario.php
@@ -1,20 +1,27 @@
-<form>
+<form method="POST">
 	<input type="hidden" name="id" value="<?php echo $tarefa['id'] ?>"/>
 	<fieldset>
 		<legend>Nova Tarefa</legend>
 		<label>
 			Tarefa:
+			<?php if ($tem_erros && isset($erros_validacao['nome'])) : ?>
+				<span class="erro">
+					<?php echo $erros_validacao['nome']; ?>
+				</span>
+			<?php endif; ?>
 			<input type="text" name="nome" value="<?php echo $tarefa['nome']; ?>"/>
 		</label>
 		<label>
 			Descrição (Opcional):
-			<textarea name="descricao">
-				<?php echo $tarefa['descricao']; ?>
+			<textarea name="descricao"><?php echo $tarefa['descricao']; ?>
 			</textarea>
 		</label>
 		<label>
 			Prazo (Opcional):
-			<input type="text" name="prazo" value="<?php echo traduz_data_para_exibir($tarefa['prazo']); ?>" />
+			<?php if($tem_erros && isset($erros_validacao['prazo'])): ?>
+			<span class="erro"><?php echo $erros_validacao['prazo']; ?></span>
+			<?php endif; ?>
+			<input type="text" name="prazo" value="<?php echo ($tarefa['id']>0)?traduz_data_para_exibir($tarefa['prazo']):$tarefa['prazo']; ?>" />
 		</label>
 		<fieldset>
 			<legend>Prioridade:</legend>
